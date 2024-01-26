7c004cee93a3d171b644c4c170fa7455c9c97f17
valer <valer@student.tugraz.at>
2024-01-26 13:16:26 +0100

    main.go new and final!

--- client/main.go
+++ client/main.go
@@ -10,6 +10,7 @@ import (
 	//"bufio"
 
 	//"os"
+	"fmt"
 	"log"
 	"teko_game/game"
 	"time"
@@ -76,16 +77,10 @@ func main() {
 	// secret := ""
 	bot := game.NewBot("5d7d39275cd5d64cb9c5f42ff45f5489b73a42ffb65bd0b8d3b3bcb3fed1d574", conn)
 
-	// for {
-	// 	bot.AutoPlay()
-
-	// 	time.Sleep(5 * time.Second)
-	// }
-	for i := 0; i < 20; i++ {
+	for i := 0; i < 15; i++ {
 		bot.AutoPlay()
-
-		// Sleep for 5 seconds before the next iteration
-		time.Sleep(5 * time.Second)
+		fmt.Println("Game", i+1, "is played")
+		time.Sleep(10 * time.Second)
 	}
 }
 
